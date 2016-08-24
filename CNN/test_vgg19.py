02fbad85aba8c10dfd62daf31188217c00dad533
timpx <timpx@users.noreply.github.com>
2016-08-24 04:46:20 -0400

    sketch changes

--- CNN/test_vgg19.py
+++ CNN/test_vgg19.py
@@ -1,11 +1,14 @@
 import numpy as np
 import tensorflow as tf
-
+from sklearn import svm
 import vgg19
 import utils
 
+# get the probabilities for all data related to a concept
+"""
 img1 = utils.load_image("./test_data/tiger.jpeg")
 img2 = utils.load_image("./test_data/puzzle.jpeg")
+"""
 
 batch1 = img1.reshape((1, 224, 224, 3))
 batch2 = img2.reshape((1, 224, 224, 3))
@@ -23,5 +26,6 @@ with tf.Session(
 
     prob = sess.run(vgg.prob, feed_dict=feed_dict)
     print(prob)
-    utils.print_prob(prob[0], './synset.txt')
-    utils.print_prob(prob[1], './synset.txt')
+
+# train a svm on the output
+
